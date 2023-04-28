fc79a959857cddbd1971ca2854ae2142bec7ddf2
emilpee <emil.petersson@dentech.se>
2023-04-28 17:08:20 +0200

    Use each loop to render currency fields

--- src/lib/Currency.svelte
+++ src/lib/Currency.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  // import { onMount } from "svelte";
+  import { onMount } from "svelte";
+  import axios from "axios";
+
   import dummyData from "../data/currencyData.json";
   import CurrencyInput from "./CurrencyInput.svelte";
   import CurrencySelector from "./CurrencySelector.svelte";
   import Output from "./Output.svelte";
-  import axios from "axios";
-  import { onMount } from "svelte";
 
   let currencies = ["SEK", "GBP", "USD"];
   const { VITE_CURRENCY_API_ENDPOINT } = import.meta.env;
@@ -27,11 +27,11 @@
   const compareCurrencies = (rate1, rate2) => rate1 / rate2;
 
   onMount(() => {
-    const [_, currencyValue] = getCurrencyVal(currency2);
+    const [_, currencyValue] = getCurrentValue(currency2);
     value2 = compareCurrencies(currencyValue, value1);
   });
 
-  const getCurrencyVal = (c: string) => {
+  const getCurrentValue = (c: string) => {
     return Object.entries(dummyData.eur).find(([currency]) => currency === c);
   };
 
@@ -40,11 +40,11 @@
 
     if (target.id === "1") {
       value1 = Number(target.value);
-      const [_, currencyValue] = getCurrencyVal(currency2);
+      const [_, currencyValue] = getCurrentValue(currency2);
       value2 = currencyValue * value1;
     } else if (target.id === "2") {
       value2 = Number(target.value);
-      const [_, currencyValue] = getCurrencyVal(currency2);
+      const [_, currencyValue] = getCurrentValue(currency2);
       value1 = compareCurrencies(value2, currencyValue);
     }
   };
@@ -54,35 +54,32 @@
 
     if (target.id === "1") {
       currency1 = target.value;
-      const [_, currencyVal] = getCurrencyVal(currency1);
+      const [_, currencyVal] = getCurrentValue(currency1);
       value1 = compareCurrencies(currencyVal, value2) * value2;
     } else if (target.id === "2") {
       currency2 = target.value;
-      const [_, currencyVal] = getCurrencyVal(currency2);
+      const [_, currencyVal] = getCurrentValue(currency2);
       value2 = compareCurrencies(currencyVal, value1) * value1;
     }
   };
 </script>
 
-<div class="card">
-  <div>
-    <CurrencySelector
-      id={"1"}
-      currencies={Object.keys(dummyData.eur)}
-      selectedCurrency={currency1}
-      on:change={(e) => onCurrencyChange(e)}
-    />
-    <CurrencyInput id={"1"} on:change={(e) => onInputChange(e)} num={value1} />
-  </div>
-  <div>
-    <CurrencySelector
-      id={"2"}
-      selectedCurrency={currency2}
-      currencies={Object.keys(dummyData.eur)}
-      on:change={(e) => onCurrencyChange(e)}
-    />
-    <CurrencyInput id={"2"} on:change={(e) => onInputChange(e)} num={value2} />
-  </div>
+<div class="currency">
+  {#each ["1", "2"] as id}
+    <div>
+      <CurrencySelector
+        {id}
+        currencies={Object.keys(dummyData.eur)}
+        selectedCurrency={id === "1" ? currency1 : currency2}
+        on:change={(e) => onCurrencyChange(e)}
+      />
+      <CurrencyInput
+        {id}
+        num={id === "1" ? value1 : value2}
+        on:change={(e) => onInputChange(e)}
+      />
+    </div>
+  {/each}
 </div>
 
 <div class="output">
@@ -90,7 +87,7 @@
 </div>
 
 <style>
-  .card {
+  .currency {
     display: flex;
     flex-direction: column;
   }
